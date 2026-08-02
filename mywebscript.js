f6ee0b0aa9e76769d20fe3337b0b0b1602a60843
gtocant <gtocant@gmail.com>
2026-08-02 19:37:32 -0300

    Create mywebscript.js

---
+++ mywebscript.js
@@ -0,0 +1,21 @@
+let RunSentimentAnalysis = () => {
+
+    textToAnalyze = document.getElementById("textToAnalyze").value;
+
+ 
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "emotionDetector?textToAnalyze=" + textToAnalyze, true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            document.getElementById("system_response").innerHTML = xhttp.responseText;
+
+        }
+
+    };
